20b4bb241b38e95845e28780a550c88f1a705f6d
robot1525 <73985895+robot1525@users.noreply.github.com>
2021-01-31 16:42:28 +0800

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,4 +1,4 @@
-ifconfig > ip_ifconfig
-curl icanhazip.com > ip_curl_icanhazip
-curl ifconfig.me > ip_curl_ifconfig
-curl ipecho.net/plain > ip_curl_ipecho
+ifconfig > ip_ifconfig.log
+curl icanhazip.com > ip_curl_icanhazip.log
+curl ifconfig.me > ip_curl_ifconfig.log
+curl ipecho.net/plain > ip_curl_ipecho.log
